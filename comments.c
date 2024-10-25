ab4af46d68faa4415330f8fe21d719ae5783bc2b
Vustron Vustronus <runa.michaeljoshua@dnsc.edu.ph>
2024-10-25 17:57:12 +0800

    first commit

---
+++ comments.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+  
+  // This is a comment
+  /*
+    This
+    is
+    a
+    multi-line
+    comment
+  */
+ 
+  printf("1\t2\t3\n4\t5\t6\n7\t8\t9");
+  
+  printf("\n\"I like Pizza\" - Abraham Lincoln");
+  
+  printf("\n\\I like Pizza\\");
+  return 0;
+}
