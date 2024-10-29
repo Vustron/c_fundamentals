31edaf75e0989d3337f443507fb0ec21f114dda0
Vustron Vustronus <runa.michaeljoshua@dnsc.edu.ph>
2024-10-29 15:28:47 +0800

    feat: constants

---
+++ constants.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+  // constants - fixed value that cannot be altered by the program during its execution
+  
+  const float pi = 3.14159;
+  
+  printf("%f", pi);
+  
+  return 0;
+}
